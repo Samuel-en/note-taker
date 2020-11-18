100ccce44112bbc01ac1414b8812281ccf5a6b9f
samiandreas70@gmail.com <samiandreas70@gmail.com>
2020-11-17 22:30:06 -0500

    created dependencies, created express instance, created port and middleware code

---
+++ server.js
@@ -0,0 +1,17 @@
+// dependencies 
+const express = require('express');
+const app = express();
+const path = require('path');
+const fs = require('fs');
+const { urlencoded } = require('body-parser');
+
+// express instance 
+const app = express();
+
+// port 
+const PORT = process.env.PORT || 8080;
+
+// middleware code
+app.use(express,urlencoded({extended:true}));
+app.use(express.json());
+app.use(express.static(path.join(__dirname, "public")));
